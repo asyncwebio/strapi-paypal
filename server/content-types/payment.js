25897efc1b4df10b6df83fa5fe4b349c8fbc36b0
nishekh-e-r <nishekh@higheredlab.com>
2022-12-09 15:38:48 +0530

    fix: hidden content-types from cme,ctb

--- server/content-types/payment.js
+++ server/content-types/payment.js
@@ -1,72 +1,72 @@
-"use strict";
+'use strict';
 
 module.exports = {
   info: {
-    tableName: "PaypalPayment",
-    singularName: "paypal-payment", // kebab-case mandatory
-    pluralName: "paypal-payments", // kebab-case mandatory
-    displayName: "Payment",
-    description: "Paypal Payment",
-    kind: "collectionType",
+    tableName: 'PaypalPayment',
+    singularName: 'paypal-payment', // kebab-case mandatory
+    pluralName: 'paypal-payments', // kebab-case mandatory
+    displayName: 'Payment',
+    description: 'Paypal Payment',
+    kind: 'collectionType',
   },
   options: {
     draftAndPublish: false,
   },
   pluginOptions: {
-    "content-manager": {
-      visible: true,
+    'content-manager': {
+      visible: false,
     },
-    "content-type-builder": {
-      visible: true,
+    'content-type-builder': {
+      visible: false,
     },
   },
   attributes: {
     txnDate: {
-      type: "datetime",
+      type: 'datetime',
       required: true,
       configurable: false,
     },
     transactionId: {
-      type: "string",
+      type: 'string',
       maxLength: 250,
       required: true,
       configurable: false,
     },
     isTxnSuccessful: {
-      type: "boolean",
+      type: 'boolean',
       default: false,
       configurable: false,
     },
     txnMessage: {
-      type: "string",
+      type: 'string',
       maxLength: 5000,
       configurable: false,
     },
     txnErrorMessage: {
-      type: "string",
+      type: 'string',
       maxLength: 250,
       configurable: false,
     },
     txnAmount: {
-      type: "decimal",
+      type: 'decimal',
       required: true,
       configurable: false,
     },
     customerName: {
-      type: "string",
+      type: 'string',
       required: true,
       configurable: false,
     },
     customerEmail: {
-      type: "string",
+      type: 'string',
       required: true,
       configurable: false,
     },
     paypalProduct: {
-      type: "relation",
-      relation: "manyToOne",
-      target: "plugin::strapi-paypal.paypal-product",
-      inversedBy: "paypalPayment",
+      type: 'relation',
+      relation: 'manyToOne',
+      target: 'plugin::strapi-paypal.paypal-product',
+      inversedBy: 'paypalPayment',
       configurable: false,
     },
   },
